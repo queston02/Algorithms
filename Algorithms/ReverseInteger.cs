e71e0e16c73dd95a34c9fa3aa49406596c804faf
queston02 <chandramani02@yahoo.co.uk>
2020-03-16 10:19:35 -0700

    Reverse Integer & ZigZagConversion

---
+++ Algorithms/ReverseInteger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class ReverseInteger
+    {
+        static void Main()
+        {
+            Console.WriteLine(Reverse(123));
+            Console.WriteLine(Reverse(-123));
+            Console.WriteLine(Reverse(120));
+            Console.WriteLine(Reverse(1534236469));
+            Console.WriteLine(Reverse(1563847412));
+        }
+
+        static int Reverse(int x)
+        {
+            var result = 0;
+            while (x != 0)
+            {
+                var reminder = x % 10;
+                x = x / 10;
+                if (result > int.MaxValue / 10 || (result == int.MaxValue / 10 && reminder > 7)) return 0;
+                if (result < int.MinValue / 10 || (result == int.MinValue / 10 && reminder < -8)) return 0;
+                result = result * 10 + reminder;
+            }
+
+            return result;
+        }
+
+        static int ReverseBin(int x)
+        {
+            var intList = new List<int>();
+            while (x != 0)
+            {
+                var reminder = x % 10;
+                intList.Add(reminder);
+                x = x / 10;
+            }
+            try
+            {
+                var result = 0;
+                for (var i = 0; i < intList.Count; i++)
+                {
+                    var multiplier = (int)Math.Pow(10, intList.Count - i - 1);
+                    if (multiplier == 1000000000 && (intList[i] > 2 || intList[i] < -2))
+                        return 0;
+                    if (result > 0 && int.MaxValue - result < intList[i] * multiplier)
+                        return 0;
+                    if (result < 0 && int.MinValue - result > intList[i] * multiplier)
+                        return 0;
+                    result += intList[i] * multiplier;
+                }
+                return result;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Algorithms/ZigZagConversion.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class ZigZagConversion
+    {
+        static void Main()
+        {
+            Console.WriteLine(Convert("PAYPALISHIRING", 0));
+            Console.WriteLine(Convert("PAYPALISHIRING", 1));
+            Console.WriteLine(Convert("PAYPALISHIRING", 2));
+            Console.WriteLine(Convert("PAYPALISHIRING", 3));
+            Console.WriteLine(Convert("PAYPALISHIRING", 4));
+            Console.WriteLine(Convert("PAYPALISHIRING", 14));
+        }
+
+        private static string Convert(string s, int numRows)
+        {
+            if (numRows == 1) return s;
+
+            StringBuilder ret = new StringBuilder();
+            int n = s.Length;
+            int cycleLen = 2 * numRows - 2;
+
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j + i < n; j += cycleLen)
+                {
+                    ret.Append(s[j + i]);
+                    if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+                        ret.Append(s[j + cycleLen - i]);
+                }
+            }
+            return ret.ToString();
+        }
+
+        private static string ConvertBin(string s, int numRows)
+        {
+            var maxIndex = numRows - 1;
+            if (maxIndex < 0)
+                return string.Empty;
+            var currentIndex = 0;
+            var stringArray = new string[numRows];
+            var forward = true;
+            for (var i = 0; i < s.Length; i++)
+            {
+                stringArray[currentIndex] = stringArray[currentIndex] + s[i];
+
+                if (currentIndex == maxIndex)
+                    forward = false;
+                if(currentIndex == 0)
+                    forward = true;
+
+                if (maxIndex == 0)
+                    currentIndex = 0;
+                else if (forward)
+                    currentIndex += 1;
+                else
+                    currentIndex -= 1;
+            }
+
+            return String.Join(string.Empty, stringArray);
+        }
+    }
+}
